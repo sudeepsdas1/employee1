d508f15fcf71bd3f00ba1784e19dbe6da3bfee83
sudeepsdas1 <sudeepsdas2002@gmail.com>
2022-11-28 08:47:50 +0530

    modifications

--- models/leave.js
+++ models/leave.js
@@ -1,19 +1,20 @@
 const mongoose = require("mongoose");
 
 var schema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  occupation: {
-    type: String,
-    required: true,
-  },
-  gender: String,
-  status: String,
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    occupation: {
+        type: String,
+        required: true,
+    },
+    gender: String,
+    status: String,
 });
+onst Userdb = mongoose.model('userdb', schema);
