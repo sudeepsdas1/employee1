fdc1f8cc3aad9cac0584a92e2e8f8e9e57869a02
sudeepsdas1 <sudeepsdas2002@gmail.com>
2022-12-01 08:47:50 +0530

    modifications

--- models/payslip.js
+++ models/payslip.js
@@ -1,4 +1,9 @@
 / Import necessary modules
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose'); // If you're using MongoDB
+const mongoose = require('mongoose'); // If you're using MongoDB\
+// Create an instance of the Express application
+const app = express();
+
+// Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
+app.use(bodyParser.json());
