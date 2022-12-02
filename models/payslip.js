753373baf57ac5cbed1fcda6b6e135ca605da6df
sudeepsdas1 <sudeepsdas2002@gmail.com>
2022-12-02 08:47:50 +0530

    modifications

--- models/payslip.js
+++ models/payslip.js
@@ -7,3 +7,8 @@ const app = express();
 
 // Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
 app.use(bodyParser.json());
+// Connect to your MongoDB (replace <db_uri> with your actual MongoDB URI)
+mongoose.connect('<db_uri>', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
