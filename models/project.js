e97a4e6de49f53deae9c5628724a94f7e9a9a0f4
sudeepsdas1 <sudeepsdas2002@gmail.com>
2022-12-06 08:47:50 +0530

    modifications

--- models/project.js
+++ models/project.js
@@ -4,3 +4,5 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 // Create an instance of the Express application
 const app = express();
+// Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
+app.use(bodyParser.json());
