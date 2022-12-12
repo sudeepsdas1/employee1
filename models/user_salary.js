37ae482e529abc0e7d9972d23f5c5c78c337abc0
sudeepsdas1 <sudeepsdas2002@gmail.com>
2022-12-12 08:47:50 +0530

    modifications

--- models/user_salary.js
+++ models/user_salary.js
@@ -7,6 +7,11 @@ const app = express();
 // Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
 app.use(bodyParser.json());
 mongoose.connect("<db_uri>", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
+// Create a Mongoose model for your employee (replace 'Employee' with your desired model name)
+const Employee = mongoose.model('Employee', EmployeeSchema);
+
+// Define a route to fetch assigned salary for an employee
+app.get('/employee/:employeeId/salary', async(req, res) => {
