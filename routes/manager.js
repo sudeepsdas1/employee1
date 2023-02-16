7115c98568758ac0c5d15dcbbb727c4e22699847
sudeepsdas1 <sudeepsdas2002@gmail.com>
2023-02-16 08:47:50 +0530

    modified topics

---
+++ routes/manager.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
