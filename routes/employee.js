002cfcbdd9764a547ea34e1cd082f6241c858700
sudeepsdas1 <sudeepsdas2002@gmail.com>
2023-03-14 08:47:50 +0530

    modifications

--- routes/employee.js
+++ routes/employee.js
@@ -3,3 +3,6 @@ const route = express.Router();
 
 const services = require("../services/render");
 const controller = require("../controller/controller");
+route.get("/", services.homeRoutes);
+
+route.get("/add-user", services.add_user);
